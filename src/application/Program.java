1dc53d8518c9f73b288b36329c0737dd058ce161
Antonio Alan <tonio.alan07@gmail.com>
2022-07-09 21:11:38 -0300

    list addition

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,39 @@
 package application;
 
+import entities.Champion;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
 
+        List<Champion> listChampions = new ArrayList<>();
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("How many champions you will add on the list? ");
+        int n = s.nextInt();
+
+        for (int i = 0; i < n; i++){
+            System.out.print("Name: ");
+            s.nextLine();
+            String name = s.nextLine();
+            System.out.print("Life: ");
+            int life = s.nextInt();
+            System.out.print("Attack: ");
+            int attack = s.nextInt();
+            System.out.print("Armor: ");
+            int armor = s.nextInt();
+            listChampions.add(new Champion(name, life, attack,armor));
+        }
+        for(Champion champ : listChampions){
+            System.out.printf("Name: %s%n" +
+                              "Life: %d%n" +
+                              "Attack: %d%n"+
+                              "Armor: %d",champ.getName(),champ.getLife(),champ.getAttack(),champ.getArmor());
+        }
+
+    s.close();
     }
 }
--- src/entities/Champion.java
+++ src/entities/Champion.java
@@ -9,48 +9,39 @@ public class Champion {
     public Champion(){
 
     }
-
     public Champion(String name, Integer life, Integer attack, Integer armor) {
         this.name = name;
         this.life = life;
         this.attack = attack;
         this.armor = armor;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public Integer getLife() {
         return life;
     }
-
     public void setLife(Integer life) {
         this.life = life;
     }
-
     public Integer getAttack() {
         return attack;
     }
-
     public void setAttack(Integer attack) {
         this.attack = attack;
     }
-
     public Integer getArmor() {
         return armor;
     }
-
     public void setArmor(Integer armor) {
         this.armor = armor;
     }
-
-    public  void takeDamage(){}
-
+    public void takeDamage(Champion enemy){
+        this.life = life - (enemy.getAttack() - armor);
+    }
     public String status(){
         return null;
     }
